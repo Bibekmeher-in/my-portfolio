38ba47672e310054c02b04ce95c8edfe4de38343
Bibekananda Meher <bibekanandam958@gmail.com>
2026-03-08 14:03:01 +0530

    fix build error

--- next.config.js
+++ next.config.js
@@ -6,25 +6,10 @@ const nextConfig = {
     images: {
         domains: ['localhost', 'res.cloudinary.com'],
         formats: ['image/avif', 'image/webp'],
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     },
     experimental: {
         optimizePackageImports: ['react-icons'],
     },
-    headers: async () => {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=31536000, immutable'
-                    }
-                ]
-            }
-        ]
-    }
 }
 
 module.exports = nextConfig
